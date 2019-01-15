c83ab090d23d78c3483a8d48896fdb1aedb756fc
khaalid abdullah <khaalidabdullah@khaalids-MacBook-Pro.local>
2019-01-14 16:19:36 -0800

    test

--- week_two/src/pages/index.js
+++ week_two/src/pages/index.js
@@ -2,21 +2,25 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 import Button from '../components/button'
 import Title from '../components/Title'
+import Body from '../components/Body'
+import Grid from '../components/Grid'
+
+
+
+
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <Title/>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>    
+    <Body/>
     <Button/>
-      <Image />
+    <Grid/>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
